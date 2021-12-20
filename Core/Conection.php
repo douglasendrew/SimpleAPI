7388ac40d25f26f1a326d5a79249d03e17b77c84
douglasendrew <suporte7.excelent@gmail.com>
2021-12-20 11:55:46 -0300

    Correção db type

--- Core/Conection.php
+++ Core/Conection.php
@@ -12,7 +12,7 @@
         private static $usuario = "root";
         private static $senha = "";
         private static $dbname = "api_integracao";
-        private static $dbtype = "mysqli";
+        private static $dbtype = "mysql";
 
         public function init()
         {
@@ -33,5 +33,5 @@
         }
 
     }
-    
+
 ?>
