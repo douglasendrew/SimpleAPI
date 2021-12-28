568d51f2ff6672b2e1bc78c065b92f125be5eb75
douglasendrew <suporte7.excelent@gmail.com>
2021-12-28 17:43:48 -0300

    Correção na conexão do banco de dados

--- Core/Conection.php
+++ Core/Conection.php
@@ -4,7 +4,7 @@
 
     use Core\Requisicao;
 
-    use \PDO;
+    use PDO;
     use PDOException;
 
     class Conection
@@ -20,7 +20,7 @@
         {
             try 
             {
-                $pdo = new \PDO(self::$dbtype . ":host=". self::$host . ";dbname=". self::$dbname, self::$usuario, self::$senha);
+                $pdo = new PDO(self::$dbtype . ":host=". self::$host . ";dbname=". self::$dbname, self::$usuario, self::$senha);
                 return $pdo;
             } catch (PDOException $e)
             {
